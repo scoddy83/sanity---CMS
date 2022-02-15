b63c5c8c8d102647e74fdde1702177d6e310cb51
scoddy83 <soutar.n@gmail.com>
2022-02-15 13:08:16 +0100

    first commit v-0.0.1

---
+++ schemas/sujet.ts
@@ -0,0 +1,57 @@
+export default {
+    title: 'Sujets',
+    name: 'sujets',
+    type: 'document',
+    fields: [
+        {
+          title: 'Jahr',
+          name: 'jahr',
+          type: 'number',
+        },
+        {
+          title: 'Datum Morgestraich',
+          name: 'datum_morgestraich',
+          type: 'date',
+        },
+        {
+          title: 'Sujet Basler Fasnacht',
+          name: 'sujet_basler_fasnacht',
+          type: 'string',
+        },
+        {
+          title: 'Sujet BGC',
+          name: 'sujet_bgc',
+          type: 'string',
+        },
+        {
+          title: 'Färs',
+          name: 'faers',
+          type: 'string',
+        },
+        {
+          title: 'Voordraab',
+          name: 'voordraab',
+          type: 'string',
+        },
+        {
+          title: 'Requisit',
+          name: 'requisit',
+          type: 'string',
+        },
+        {
+          title: 'Major',
+          name: 'major',
+          type: 'string',
+        },
+        {
+          title: 'Spiil',
+          name: 'spiil',
+          type: 'string',
+        },
+        {
+          title: 'Sujet Bild',
+          name: 'sujet_bild',
+          type: 'image',
+        }
+    ]
+}
